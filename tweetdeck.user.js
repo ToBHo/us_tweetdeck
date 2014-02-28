81766322f88933da2cde69eb90d4d2d274273f9d
Tobias Holz <info@tobho.de>
2014-02-28 17:43:20 +0100

    fixed Script to add CSS to head and changed CSS-file to be FF/Chrome compatible

--- tweetdeck.user.js
+++ tweetdeck.user.js
@@ -1,13 +1,23 @@
 // ==UserScript==
 // @name        TobHo Tweetdeck
-// @namespace   td
+// @namespace   de.tobho
 // @description Tweetdeck Enhancements
 // @include     https://tweetdeck.twitter.com/
+// @include     http://tweetdeck.twitter.com/
 // @version     1
-// @grant       GM_addStyle
-// @resource    style https://github.com/ToBHo/us_tweetdeck/blob/master/tweetdeck_enhancements.css
+// @grant       none
 // ==/UserScript==
-var cssTxt = GM_getResourceText('style');
-GM_addStyle(cssTxt);
 
-GM_log('Style should be added by now :)');
+var styleOrigin = 'https://github.com/ToBHo/us_tweetdeck/raw/master/tweetdeck_enhancements.css';
+
+addStyleSheet = function(origin){
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.setAttribute('rel','stylesheet');
+    link.setAttribute('href', origin);
+    link.setAttribute('type', 'text/css');
+    head.appendChild(link);
+}
+
+addStyleSheet(styleOrigin);
+console.log('Style should be added by now :)');
